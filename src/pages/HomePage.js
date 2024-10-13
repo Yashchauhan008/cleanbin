13f3871ca8d67ee4e7843da451f2f3c920943f27
yash <yashchauhan1775@gmail.com>
2024-10-13 21:18:53 +0530

    submission qualification

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,19 +4,19 @@ import DustbinRequests from "../components/DustbinRequests";
 import UserList from "../components/UserList";
 
 const HomePage = () => {
-  const [list, setList] = useState("dustbinList"); // Corrected setter function
+  // const [list, setList] = useState("dustbinList"); // Corrected setter function
 
   return (
     <>
-      <div>HomePage</div>
-      <button onClick={() => setList("dustbinList")}>Dustbin List</button>
+      {/* <div>HomePage</div> */}
+      {/* <button onClick={() => setList("dustbinList")}>Dustbin List</button>
       <button onClick={() => setList("userList")}>User List</button>
-      <button onClick={() => setList("dustbinRequests")}>Dustbin Requests</button>
+      <button onClick={() => setList("dustbinRequests")}>Dustbin Requests</button> */}
 
       {/* Conditional rendering */}
-      {list === "dustbinList" && <DustbinList/>}
-      {list === "dustbinRequests" && <DustbinRequests/>}
-      {list === "userList" && <UserList/>}
+      <DustbinList/>
+      {/* {list === "dustbinRequests" && <DustbinRequests/>} */}
+      {/* {list === "userList" && <UserList/>} */}
     </>
   );
 };
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,11 +4,12 @@ import { useNavigate } from 'react-router-dom'
 const LandingPage = () => {
     const navigate = useNavigate()
   return (
-    <div>
-      <h1>Welcome to My React App!</h1>
-      <p>This is a simple landing page.</p>
-      <button onClick={()=>{navigate("/bin/101")}}>report bin</button>
-      <button onClick={()=>{navigate("/home")}}>home</button>
+    <div  style={{color:'rgb(166, 85, 229)'}}>
+      <h1>Test Page</h1>
+      <p>Click to heck what happen on qr code scane</p>
+      <button onClick={()=>{navigate("/bin/101")}}>demo</button>
+      <p>dustbin details for all type</p>
+      <button onClick={()=>{navigate("/home")}}>Dustbin Details</button>
     </div>
   )
 }
